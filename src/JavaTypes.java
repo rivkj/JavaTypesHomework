a8e2a8eb9cb5ee677e4a9b590678ba6fb963dd71
Kimberly Rivas <rivaskj07@gmail.com>
2022-06-22 21:27:46 -0400

    added files to repo

---
+++ src/JavaTypes.java
@@ -0,0 +1,20 @@
+
+
+public class JavaTypes {
+	public static void main(String[] args) {
+	int myNum = 5;
+	System.out.println(myNum);
+	
+	float myFloatNum = 5.67f;
+	System.out.println(myFloatNum);
+	
+	char myLetter = 'A';
+	System.out.println(myLetter);
+	
+	boolean myBoo = true;
+	System.out.println(myBoo);
+	
+	String myExample = "Hello Example";
+	System.out.println(myExample);
+	}
+}
